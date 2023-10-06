9e70af8da76de3726b4c16e5e4168a6b33f7b2de
Isaac Alencar <alencarisaac3@gmail.com>
2023-10-06 12:54:29 -0300

    setup chakra ui

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,20 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+import type { AppProps } from "next/app";
+
+import "@fontsource/roboto";
+
+export const theme = extendTheme({
+  fonts: {
+    body: "Roboto, sans-serif",
+    heading: "Georgia, serif",
+    mono: "Menlo, monospace",
+  },
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider>
+      <Component {...pageProps} theme={theme} />
+    </ChakraProvider>
+  );
 }
