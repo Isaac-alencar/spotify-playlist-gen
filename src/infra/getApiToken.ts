9f620e991f2c21496afc3fdf023d2579d5ff4c0f
Isaac Alencar <alencarisaac3@gmail.com>
2023-10-11 12:19:55 -0300

    move away function getApiToken from searchTrack

---
+++ src/infra/getApiToken.ts
@@ -0,0 +1,23 @@
+export const getAuthToken = async () => {
+  const CLIENT_ID = process.env.NEXT_PUBLIC_SPOTIFY_CLIENT_ID;
+  const CLIENT_SECRET = process.env.NEXT_PUBLIC_SPOTIFY_CLIENT_SECRET;
+
+  const BASE_URL = "https://accounts.spotify.com/api/token";
+
+  const credentials = btoa(`${CLIENT_ID}:${CLIENT_SECRET}`);
+
+  const requestOptions = {
+    method: "POST",
+    headers: {
+      Authorization: `Basic ${credentials}`,
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+    body: "grant_type=client_credentials",
+  };
+
+  const response = await fetch(BASE_URL, requestOptions);
+
+  const data = await response.json();
+
+  return { token: data.access_token };
+};
--- src/infra/searchTrack.ts
+++ src/infra/searchTrack.ts
@@ -1,38 +1,6 @@
-import { Track, createTrack } from "@/domain/Track";
-
-type Artist = {
-  href: string;
-  id: string;
-  name: string;
-  type: "artist";
-  uri: string;
-};
-
-type Album = {
-  name: string;
-  images: {
-    url: string;
-  }[];
-};
-
-type Results = {
-  id: string;
-  name: string;
-  album: Album;
-  artists: Artist[];
-};
-
-type SpotifyAPIResponse = {
-  tracks: {
-    href: string;
-    items: Results[];
-    limit: number;
-    next: string;
-    offset: number;
-    previous: string | null;
-    total: number;
-  };
-};
+import { Track } from "@/domain/Track";
+import { getAuthToken } from "./getApiToken";
+import { mapToDomainFormat } from "./mapToDomainFormat";
 
 export const searchTrack = async (searchTerm: string): Promise<Track[]> => {
   const { token } = await getAuthToken();
@@ -50,43 +18,5 @@ export const searchTrack = async (searchTerm: string): Promise<Track[]> => {
   );
   const data = await response.json();
 
-  return mapToDomainFormat(data);
-};
-
-export const getAuthToken = async () => {
-  const CLIENT_ID = process.env.NEXT_PUBLIC_SPOTIFY_CLIENT_ID;
-  const CLIENT_SECRET = process.env.NEXT_PUBLIC_SPOTIFY_CLIENT_SECRET;
-
-  const BASE_URL = "https://accounts.spotify.com/api/token";
-
-  const credentials = btoa(`${CLIENT_ID}:${CLIENT_SECRET}`);
-
-  const requestOptions = {
-    method: "POST",
-    headers: {
-      Authorization: `Basic ${credentials}`,
-      "Content-Type": "application/x-www-form-urlencoded",
-    },
-    body: "grant_type=client_credentials",
-  };
-
-  const response = await fetch(BASE_URL, requestOptions);
-
-  const data = await response.json();
-
-  return { token: data.access_token };
-};
-
-const mapToDomainFormat = (data: SpotifyAPIResponse): Track[] => {
-  const tracks = data.tracks.items.map((item) => {
-    return createTrack({
-      id: item.id,
-      name: item.name,
-      album: item.album.name,
-      albumCover: item.album.images[0].url,
-      artist: item.artists[0].name,
-    });
-  });
-
-  return tracks;
+  return mapToDomainFormat(data.tracks.items);
 };
