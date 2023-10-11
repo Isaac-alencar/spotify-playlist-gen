39583365d64cf024295475dfca91e4d92592d100
Isaac Alencar <alencarisaac3@gmail.com>
2023-10-11 12:52:41 -0300

    create home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,49 @@
-import { Flex, Heading } from "@chakra-ui/react";
+import { Button, Flex, Heading, Text } from "@chakra-ui/react";
+import { useRouter } from "next/router";
+import React from "react";
 
 export default function Home() {
+  const { push } = useRouter();
+
   return (
-    <Flex flex="1" h="100vh" bg="gray.800">
-      <Heading color="blue.500">Hello, World</Heading>
+    <Flex
+      h="50vh"
+      alignItems="center"
+      justifyContent="center"
+      flexDirection="column"
+    >
+      <Heading
+        as="h1"
+        size="4xl"
+        bgGradient="linear(to-r, #1DB954, #00BFFF)"
+        bgClip="text"
+        lineHeight="taller"
+      >
+        Spotify Playlist Generator
+      </Heading>
+      <Text
+        color="whiteAlpha.600"
+        fontWeight="bold"
+        fontSize="medium"
+        maxW="40%"
+        textAlign="center"
+        mb="10"
+      >
+        Unleash Your Musical Adventure Let Your Favorite Song Lead the Way. Your
+        Personal Playlist Maestro. Create Playlists Tailored to Your Taste. Find
+        Your Perfect Soundtrack Today!
+      </Text>
+      <Button
+        boxShadow="2xl"
+        colorScheme="green"
+        bgGradient="linear(to-r, #1DB954, #00BFFF)"
+        _hover={{
+          bgGradient: "linear(to-r, #00BFFF, #1DB954)",
+        }}
+        onClick={() => push("search")}
+      >
+        Let&rsquo;s create one 🔥
+      </Button>
     </Flex>
   );
 }
